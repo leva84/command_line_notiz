930d322cbaca6af07426ef066320806596e18fd1
leva84 <denfrut8@gmail.com>
2020-09-04 13:42:37 +0300

    working method 'result'

--- lib/methods.rb
+++ lib/methods.rb
@@ -1,3 +1,8 @@
 def result(command)
-
+  output = IO.popen("#{command}")
+  answer = output.read.strip
+  output.close
+  answer
 end
+
+
--- main.rb
+++ main.rb
@@ -1,11 +1,16 @@
+require 'io/console'
 require_relative 'lib/methods'
 
 # шаг1: Приветствие для пользователя и краткая инструкция.
 puts "Введите команду либо введите 'help' для просмотра доступных команд, 'exit' для выхода"
 
 # шаг2: Принять, запрос пользователя.
-print '>'
-command = gets
+command = nil
+
+#while command == '' || command == 'exit'
+  print '>'
+  command = STDIN.gets
+  #end
 
 # шаг3: Результат работы команды, переход к шагу 1
-result(command)
+p result(command)
