55b2848a16e9beeef806a1b5777b4cc06aa8c965
leva84 <denfrut8@gmail.com>
2020-10-01 21:05:35 +0300

    / exampl_of_use / working example of use(english word list)  / working version 1.4 /

--- lib/english_word_list.rb
+++ lib/english_word_list.rb
@@ -15,14 +15,54 @@ class EnglishWordList < Command
     'программа выводит английские слова и их перевод для изучения'
   end
 
-  def command_work
-    arr_word = []
-    word_list.each do |string_word|
-      string_word.chomp!
-      words = string_word.split(' ')
-      arr_word << words[0]
+  def word_hash
+    h_words = {}
+    word_list.each do |string|
+      string.chomp!
+      string = string.split(' ') - ['-']
+      h_words[string[0]] = string[1..string.size]
     end
 
-    arr_word
+    h_words
+  end
+
+  def instruction
+    <<~END
+      ======================================
+      Введите перевод на английском
+      Введите enter для просмотра перевода
+      Введите exit для выхода
+      ======================================
+    END
+  end
+
+  def result_translation(input, word)
+    if input == 'exit'
+      App.new.start
+    elsif input == ''
+      puts 'translation of this word:'
+      puts word
+    elsif input != word
+      puts 'It is not right'
+      puts 'that is right:'
+      puts word
+    else
+      puts 'Yes!)'
+    end
+  end
+
+  def words_of_translation
+    word_hash.each do |word, translation|
+      puts instruction
+      puts translation
+
+      input = gets.downcase.chomp
+
+      result_translation(input, word)
+    end
+  end
+
+  def command_work
+    words_of_translation
   end
 end
